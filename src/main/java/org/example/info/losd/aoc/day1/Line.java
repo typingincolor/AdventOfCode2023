fce358da2b9a19ec06861327acd8ff48a6a34263
Andrew Braithwaite <andrew@losd.info>
2023-12-01 10:56:12 +0100

    Get Line working

--- src/main/java/org/example/info/losd/aoc/day1/Line.java
+++ src/main/java/org/example/info/losd/aoc/day1/Line.java
@@ -1,10 +1,28 @@
 package org.example.info.losd.aoc.day1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
+    private String str;
+
     public Line(String s) {
+        this.str = s;
     }
 
     public int calculateCalibrationValue() {
-        return -1;
+        StringBuilder calibrationValue = new StringBuilder();
+
+        List<Character> digits = new ArrayList<>();
+
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digits.add(c);
+            }
+        }
+
+        calibrationValue.append(digits.getFirst()).append(digits.getLast());
+
+        return Integer.parseInt(calibrationValue.toString());
     }
 }
--- src/test/java/info/losd/aoc/day1/LineTest.java
+++ src/test/java/info/losd/aoc/day1/LineTest.java
@@ -1,6 +1,5 @@
 package info.losd.aoc.day1;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.example.info.losd.aoc.day1.Line;
 
@@ -8,9 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LineTest {
     @Test
-    @DisplayName("Check: 1abc2")
-    void testSimpleTestCase(){
+    void testDigitsAtBeginningAndEnd(){
         Line line = new Line("1abc2");
         assertEquals(12, line.calculateCalibrationValue(), "Expecting 12");
+    }
+
+    @Test
+    void testDigitsInMiddle(){
+        Line line = new Line("pqr3stu8vwx");
+        assertEquals(38, line.calculateCalibrationValue(), "Expecting 38");
+    }
+
+    @Test
+    void testMultipleDigits(){
+        Line line = new Line("a1b2c3d4e5f");
+        assertEquals(15, line.calculateCalibrationValue(), "Expecting 15");
+    }
+
+    @Test
+    void testSingleDigit(){
+        Line line = new Line("treb7uchet");
+        assertEquals(77, line.calculateCalibrationValue(), "Expecting 77");
     }
 }
